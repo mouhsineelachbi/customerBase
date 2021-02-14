b848215a5523f5b800c0c061c2be94a5a377e192
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-02-14 23:09:13 +0100

    added libraries for graphql schema

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
-
+const expressGraphQL = require('express-graphql');
+const schema = require('schema');
 const app = express();
-
-app.listen(3000, ()=>console.log("Server is listening on port 3000"));
+app.use('/graphql', expressGraphQL({
+    schema: schema,
+    graphiql:true
+}))
+app.listen(4000, ()=>console.log("Server is listening on port 3000"));
