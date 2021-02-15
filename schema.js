2da7d7f90bb442161f017ad7ea90a52eec0d6581
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-02-15 16:58:53 +0100

    fetching data from json-server

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -7,12 +8,7 @@ const {
     GraphQLNonNull
 } = require('graphql');
 
-//  HardCoded Data
-const customers = [
-    {id: '1', name: 'John Doe', email:"johndoe@gmail.com", age:34},
-    {id: '2', name: 'Steve Smith', email:"SteveSmith@gmail.com", age:25},
-    {id: '3', name: 'Sara Wiliam', email:"sarawilliam@gmail.com", age:32},
-]
+
 //  Custom Type
 const CustomerType = new GraphQLObjectType({
     name: 'Customer',
@@ -33,14 +29,18 @@ const RootQuery = new GraphQLObjectType({
                 id: {type: GraphQLString}
             },
             resolve(parentValue, args) {
-                for(let i = 0; i<customers.length; i++)
-                {
-                    if (customers[i].id == args.id){
-                         return customers[i];
-                    }
-                }
+                return axios.get(`http://localhost:3000/customers/${args.id}`)
+                    .then(res => res.data)
+            }
+        },
+        customers: {
+            type: new GraphQLList(CustomerType),
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/customers/`)
+                    .then(res => res.data)
             }
         }
+
     }
 })
 
