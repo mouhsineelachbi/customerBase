e1db4b1f4e07916bd707235e4f46680500570f2f
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-02-16 01:09:38 +0100

    add Adding, Editting and Deleting mutations to the schema

--- schema.js
+++ schema.js
@@ -60,6 +60,29 @@ const mutation = new GraphQLObjectType({
                         age: args.age
                 }).then(res => res.data);
             }
+        },
+        deleteCustomer: {
+            type: CustomerType,
+            args: {
+                id: {type: new GraphQLNonNull(GraphQLString)},
+            },
+            resolve(parentValue, args) {
+                return axios.delete('http://localhost:3000/customers/'+args.id)
+                    .then(res => res.data)
+            }
+        },
+        editCustomer: {
+            type: CustomerType,
+            args: {
+                id: {type: new GraphQLNonNull(GraphQLString)},
+                name: {type: GraphQLString},
+                email: {type: GraphQLString},
+                age: {type: GraphQLInt}
+            },
+            resolve(parentValue, args) {
+                return axios.patch('http://localhost:3000/customers/'+args.id, args)
+                    .then(res => res.data)
+            }
         }
     }
 })
