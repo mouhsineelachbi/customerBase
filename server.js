8d6b30573dd1b80b17d7f46b9ca72c85f9298f2d
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-02-16 18:19:15 +0100

    testing get /

--- server.js
+++ server.js
@@ -6,4 +6,12 @@ app.use('/graphql', graphqlHTTP({
     schema: schema,
     graphiql:true
 }))
+
+app.get("/", function (req, res) {
+    return res.send("Hello World");
+});
+
+
 app.listen(4000, ()=>console.log("Server is listening on port 4000"));
+
+module.exports = app;
